a6fa7298a3d5d28dfa1918a853fb0fd1d1c986c0
jiangyu1211@gmail.com <jiangyu1211@gmal.com>
2015-03-12 15:58:29 +0800

    avoid sort

--- src/main/scala/com/dj/Detecter.scala
+++ src/main/scala/com/dj/Detecter.scala
@@ -32,20 +32,23 @@ object Detecter {
       iter.map(tpl => (file.getPath.toString,tpl._2.toString))
     }
 
-    implicit val caseInsensitiveOrdering = new Ordering[String] {
-      override def compare(x: String, y: String) = y.split("/")(6).split("_")(2).toLong.
-        compareTo(x.split("/")(6).split("_")(2).toLong)
-    }
+//    implicit val caseInsensitiveOrdering = new Ordering[String] {
+//      override def compare(x: String, y: String) = y.split("/")(6).split("_")(2).toLong.
+//        compareTo(x.split("/")(6).split("_")(2).toLong)
+//    }
 
-    val last = fileAndLine.sortBy({x=>x._1.split("/")(6).split("_")(2).toLong},false).first()
 
     val calToday = Calendar.getInstance().getTime
-    val format = new SimpleDateFormat("yyyy_MM_dd")
+    val format = new SimpleDateFormat("yyyy-MM-dd")
     val lastFileName = format.format(calToday)
 
-    sc.makeRDD(last.toString()).saveAsTextFile(dir+"/"+lastFileName)
+    val calYes = Calendar.getInstance()
+    calYes.add(Calendar.DATE,-1)
+    val yesterday = format.format(calYes.getTime)
 
-    val all = fileAndLine.filter{case(name,line) => line.contains("ShuffleError")}
-    all.saveAsTextFile(dir+"/"+lastFileName+"_result")
+
+    val pattern = ("^"+yesterday+".*(java.lang.OutOfMemoryError|ShuffleError).*").r.pattern
+    val all = fileAndLine.filter{case(name,line) => pattern.matcher(line).matches}.repartition(1)
+    all.saveAsTextFile(dir+"/"+lastFileName)
   }
 }
